04af06d511a8b560d381ca7da8cbed4a250a74e2
Sandhya Kumari <sandhyakumari7784011128@gmail.com>
2025-07-30 10:54:56 +0530

    Initial commit

---
+++ Typecasting/ImplicitCasting.java
@@ -0,0 +1,13 @@
+public class ImplicitCasting {
+    public static void main(String[] args){
+        int i=6;
+        long l;
+        l = i;
+        double d;
+        d = i;
+        System.out.println("Original i="+i);
+        System.out.println("Casting in long="+l);
+        System.out.println("Casting in double="+d);
+
+    }
+}
