1223f73d69c982fed251bcb7f6ccd63a9a58a90d
sandhu2702 <sk0581822@gmail.com>
2025-08-03 17:21:27 +0530

    Added Logical operators

---
+++ Operator/Logical.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Logical{
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+
+        //logical operations
+        System.out.println("x && y: " + (x && y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("!x: " + (!x));
+
+    }
+
+
+}
