e34e44857828252d660deb3f8cbd43a93c00e18c
sandhu2702 <sk0581822@gmail.com>
2025-08-03 16:58:20 +0530

    Added Relation operators

---
+++ Operator/Relational.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class Relational {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a=sc.nextInt();
+        System.out.println("Enter b: ");
+        int b=sc.nextInt();
+        System.out.println("a>b: "+(a > b));
+        System.out.println("a>b: "+(b > a));
+        System.out.println("a>b: "+(b >= a));
+        System.out.println("a>b: "+(a >=b));
+        System.out.println("a>b: "+(a == b));
+        System.out.println("a>b: "+(a != b));
+
+    }
+}
