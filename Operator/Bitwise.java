a2f85d1f5d9e2ba76db45f4c61e300c9b5b6d46c
sandhu2702 <sk0581822@gmail.com>
2025-08-03 18:18:13 +0530

    Added Shift  operators

---
+++ Operator/Bitwise.java
@@ -0,0 +1,11 @@
+public class Bitwise {
+    public static void main(String[] args){
+        int d=0b1010;
+        int c=0b1100;
+
+        System.out.println("d & c : "+ (d & c));
+        System.out.println("d | c : "+ (d | c));
+        System.out.println("d ^ c : "+ (d ^ c));
+        System.out.println("~d : "+ (~d));
+    }
+}
---
+++ Operator/Shift.java
@@ -0,0 +1,7 @@
+public class Shift {
+    public static void main(String[] args){
+        int a=10;
+        System.out.println("a<<1 : "+ (a<<1));
+        System.out.println("a>>1 : "+ (a>>1));
+    }
+}
