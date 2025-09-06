e56fc457b7debc2d458d5546b65e72db2f4a02cf
sandhu2702 <sk0581822@gmail.com>
2025-09-06 08:16:36 +0530

    basic declaration of array

---
+++ Arrays/BasicDeclaration.java
@@ -0,0 +1,10 @@
+public class BasicDeclaration {
+    public static void main(String[] args){
+        int[] arr={40,66,8,9,30,27,54};
+        int n=arr.length;
+        System.out.println(n);
+        for (int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+}
